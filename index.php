792ebaf549140a56f51f6c133eaad2a3d11049b2
Emmanuel Cobian <emmanuelcobian@la-MacBook-Pro-de-Emmanuel-225.local>
2019-05-04 12:21:13 -0500

    contenido de contactos mas estilos

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <div class="bg-amarillo contenedor sombra">
     <form id="contacto" action="#">
-        <legend>Añada un contacto <span>Todos los campos son obligatorios</span></legend>
+        <legend>Añada un contacto <span> Todos los campos son obligatorios</span> </legend>
 
         <div class="campos">
             <div class="campo">
@@ -21,11 +21,76 @@
                 <label for="telefono">Telefono:</label>
                 <input type="tel" placeholder="Número de telefono"" id="telefono">
             </div>
-            <div class="campo enviar">
+        </div>
+        <div class="campo enviar">
                 <input type="submit" value="Añadir">
             </div>
-        </div>
     </form>
+</div>
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
+
+        <input type="text" id="buscar" class="buscador sombra"placeholder="Buscar contactos...">
+
+        <p class="total-contactos"><span>2</span> Contactos</p>
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Telefono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Emmanuel</td>
+                        <td>SMG</td>
+                        <td>3211064551</td>
+                        <td> 
+                            <a class="btn-editar btn" href="#">
+                                <i class="far fa-edit"></i>
+                            </a>
+
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Emmanuel</td>
+                        <td>SMG</td>
+                        <td>3211064551</td>
+                        <td> 
+                            <a class="btn-editar btn" href="#">
+                                <i class="far fa-edit"></i>
+                            </a>
+
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Emmanuel</td>
+                        <td>SMG</td>
+                        <td>3211064551</td>
+                        <td> 
+                            <a class="btn-editar btn" href="#">
+                                <i class="far fa-edit"></i>
+                            </a>
+
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 <?php include 'inc/layout/footer.php';?>
